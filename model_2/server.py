a32272af51fe78e0a653987ef19ae27e47dd5eff
urdhanaka <urdhanaka.aptanagi@gmail.com>
2024-10-23 00:12:34 +0700

    feat: update model-2 server

--- model_2/server.py
+++ model_2/server.py
@@ -4,8 +4,16 @@ import sys
 import zmq
 
 
+class RETURN_VALUE:
+    ERR_STRING = "ERROR"
+    SUCCESS_STRING = "SUCCESS"
+
+    ERR_BYTES = ERR_STRING.encode()
+    SUCCESS_BYTES = SUCCESS_STRING.encode()
+
+
 class Server:
-    SHARE_DIR = os.getcwd() + os.sep + "shared"
+    SHARED_DIR = os.getcwd() + os.sep + "shared"
 
     def __init__(self, machine_name: str, endpoint: str) -> None:
         self.__machine_name = machine_name
@@ -13,12 +21,27 @@ class Server:
         self.__context = zmq.Context()
         self.__server = self.__context.socket(zmq.REP)
 
+        self.__server.setsockopt(zmq.LINGER, 0)
+
         self.__server.bind(endpoint)
 
     def destroy(self) -> None:
         self.__server.setsockopt(zmq.LINGER, 0)
         self.__server.close()
         self.__context.term()
+
+    def list_all_files(self, include_hidden=False) -> list[bytes]:
+        all_files: list[bytes] = []
+
+        for _, _, files in os.walk(self.SHARED_DIR):
+            for file in files:
+                # check if file starts with dot (hidden)
+                if file.startswith(".") and not include_hidden:
+                    continue
+
+                all_files.append(file.encode())
+
+        return all_files
 
     def start(self) -> None:
         print(f"I: Starting server at {self.__endpoint}")
@@ -31,17 +54,83 @@ class Server:
 
             assert len(msg) == 2
 
+            print(f"I: Received message: {msg}")
+
             address = msg[0]
-            reply = [address, b"OK"]
-            self.__server.send_multipart(reply)
+            command = msg[1].decode()
+
+            if command == "HEALTH":
+                self.__server.send_multipart(
+                    [
+                        address,
+                        self.__machine_name.encode(),
+                        RETURN_VALUE.SUCCESS_BYTES,
+                        b"OK",
+                    ]
+                )
+            elif command == "LIST":
+                files = self.list_all_files()
+                self.__server.send_multipart(
+                    [address, self.__machine_name.encode(), RETURN_VALUE.SUCCESS_BYTES]
+                    + files
+                )
+            elif command == "LIST_ALL":
+                files = self.list_all_files(include_hidden=True)
+                self.__server.send_multipart(
+                    [address, self.__machine_name.encode(), RETURN_VALUE.SUCCESS_BYTES]
+                    + files
+                )
+            elif command == "DOWNLOAD":
+                if len(msg) != 3:
+                    self.__server.send_multipart(
+                        [
+                            address,
+                            self.__machine_name.encode(),
+                            RETURN_VALUE.ERR_BYTES,
+                            b"Filename not provided",
+                        ]
+                    )
+                    continue
+
+                filename = msg[3].decode()
+
+                if not os.path.exists(self.SHARED_DIR + os.sep + filename):
+                    self.__server.send_multipart(
+                        [
+                            address,
+                            self.__machine_name.encode(),
+                            RETURN_VALUE.ERR_BYTES,
+                            b"File not found",
+                        ]
+                    )
+                    continue
+
+                self.__server.send_multipart(
+                    [address, self.__machine_name.encode(), RETURN_VALUE.SUCCESS_BYTES]
+                )
+
+                with open(self.SHARED_DIR + os.sep + filename, "rb") as f:
+                    data = f.read(1024)
+                    while data:
+                        self.__server.send_multipart([data])
+                        data = f.read(1024)
+            else:
+                self.__server.send_multipart(
+                    [
+                        address,
+                        self.__machine_name.encode(),
+                        RETURN_VALUE.ERR_BYTES,
+                        b"Invalid command",
+                    ]
+                )
 
         self.destroy()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("I: Syntax: %s <endpoint>" % sys.argv[0])
+    if len(sys.argv) < 3:
+        print("I: Syntax: %s <machine_name> <endpoint>" % sys.argv[0])
         sys.exit(0)
 
-    server = Server("mesin_1", sys.argv[1])
+    server = Server(sys.argv[1], sys.argv[2])
     server.start()

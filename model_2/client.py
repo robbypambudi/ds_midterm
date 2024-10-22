c03b25aa11652aa5d3a4bf2113887bc193bcbfce
Robby Pambudi <robby.pambudi10@gmail.com>
2024-10-23 00:10:02 +0700

    feat: update client model 2

--- model_2/client.py
+++ model_2/client.py
@@ -1,78 +1,96 @@
-#
-# Freelance Client - Model 2
-# Uses DEALER socket to blast one or more services
-#
-# Author: Daniel Lundin <dln(at)eintr(dot)org>
-#
-
-import sys
 import time
-
 import zmq
+from random import randint
 
-GLOBAL_TIMEOUT = 2500  # ms
+class Client: 
+  MAX_RETRIES = 10
+  MAX_REQUESTS_TIMEOUT = 3000
 
+  def __init__(self, servers):
+    self.servers = 0
+    self.sequence = 0
+    self.context = zmq.Context()
+    self.socket = self.context.socket(zmq.DEALER)
+    self.client_id = "%04X-%04X" % (randint(0, 0x10000), randint(0, 0x10000))
 
-class FLClient(object):
-    def __init__(self):
-        self.servers = 0
-        self.sequence = 0
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.DEALER)  # DEALER
+    
+  def destroy(self):
+    self.socket.setsockopt(zmq.LINGER, 0)  # Terminate early
+    self.socket.close()
+    self.context.term()
+  
+  def connect(self, endpoint):
+    self.socket.connect(endpoint)
+    self.servers += 1
+    print("%s: Connected to %s" % (self.client_id, endpoint))
+  
+  def request(self, *request):
+    self.sequence += 1
+    msg = [b"", str(self.sequence).encode()] + list(request)
+    
+    for server in range(self.servers):
+        self.socket.send_multipart(msg)
 
-    def destroy(self):
-        self.socket.setsockopt(zmq.LINGER, 0)  # Terminate early
-        self.socket.close()
-        self.context.term()
+    poll = zmq.Poller()
+    poll.register(self.socket, zmq.POLLIN)
 
-    def connect(self, endpoint):
-        self.socket.connect(endpoint)
-        self.servers += 1
-        print("I: Connected to %s" % endpoint)
+    socks = dict(poll.poll(self.MAX_REQUESTS_TIMEOUT))
+    
+    reply = None
+    endtime = time.time() + self.MAX_REQUESTS_TIMEOUT / 1000
+    while time.time() < endtime:
+      socks = dict(poll.poll((endtime - time.time()) * 1000))
+      if socks.get(self.socket) == zmq.POLLIN:
+        reply = self.socket.recv_multipart()
+        # assert len(reply) == 3
+        sequence = int(reply[1])
+        if sequence == self.sequence:
+          break
+    return reply
 
-    def request(self, *request):
-        # Prefix request with sequence number and empty envelope
-        self.sequence += 1
-        msg = [b"", str.encode(str(self.sequence))] + list(request)
+if __name__ == '__main__':
+  server_list = [
+    "tcp://192.168.1.43:5555",
+    "tcp://192.168.1.43:5556",
+    "tcp://192.168.1.43:5557",
+  ]
+  
+  client = Client(servers=server_list)
+  
+  endpoint = len(server_list) - 1
+  print("%s: Trying to connect to server..." % client.client_id)
 
-        # Blast the request to all connected servers
-        for server in range(self.servers):
-            self.socket.send_multipart(msg)
-
-        # Wait for a matching reply to arrive from anywhere
-        # Since we can poll several times, calculate each one
-        poll = zmq.Poller()
-        poll.register(self.socket, zmq.POLLIN)
-
-        reply = None
-        endtime = time.time() + GLOBAL_TIMEOUT / 1000
-        while time.time() < endtime:
-            socks = dict(poll.poll((endtime - time.time()) * 1000))
-            if socks.get(self.socket) == zmq.POLLIN:
-                reply = self.socket.recv_multipart()
-                assert len(reply) == 3
-                sequence = int(reply[1])
-                if sequence == self.sequence:
-                    break
-        return reply
-
-
-if len(sys.argv) == 1:
-    print("I: Usage: %s <endpoint> ..." % sys.argv[0])
-    sys.exit(0)
-
-# Create new freelance client object
-client = FLClient()
-
-for endpoint in sys.argv[1:]:
-    client.connect(endpoint)
-
-start = time.time()
-for requests in range(10000):
-    request = b"random name"
-    reply = client.request(request)
-    if not reply:
-        print("E: Name service not available, aborting")
-        break
-print("Average round trip cost: %i usec" % ((time.time() - start) / 100))
-client.destroy()
+  for server in server_list:
+      client.connect(server)
+  
+  print("List of commands: ")
+  print("- HEALTH")
+  print("- LIST")
+  print("- LIST_ALL")
+  print("- DOWNLOAD <filename>")
+  print("- EXIT")
+  while True:
+      command = input("Enter your message: ")
+      if (input == "EXIT"):
+          raise Exception("Exiting the program")
+      
+      if command.startswith("DOWNLOAD"):
+          command = command.split(" ")
+          reply = client.request(command[0].encode(), command[1].encode())
+          
+          with open(f"client_{command[1]}", "wb") as f:
+              f.write(reply[0])
+          
+          print(f"Downloaded {command[1]}")
+          continue
+      else:
+          reply = client.request(command.encode())   
+          if not reply:
+              print("E: No response from server")
+              break
+          for i in range(len(reply)):
+              if i == 0:
+                  continue
+              else:
+                  print("Message: %s" % reply[i].decode())
+    

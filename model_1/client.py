64fa2faefb05a88ca2f09190a48922c8dea577fa
urdhanaka <urdhanaka.aptanagi@gmail.com>
2024-10-25 02:50:11 +0700

    feat: update model-1

--- model_1/client.py
+++ model_1/client.py
@@ -1,91 +1,124 @@
-import zmq
+import os
+import sys
 from random import randint
 
-class Client: 
-  MAX_RETRIES = 10
-  MAX_REQUESTS_TIMEOUT = 3000
+import zmq
 
-  def __init__(self):
-    self.servers = 0
-    self.sequence = 0
-    self.context = zmq.Context()
-    self.socket = self.context.socket(zmq.REQ) 
-    self.client_id = "%04X-%04X" % (randint(0, 0x10000), randint(0, 0x10000))
 
-    
-  def destroy(self):
-    self.socket.setsockopt(zmq.LINGER, 0)  # Terminate early
-    self.socket.close()
-    self.context.term()
-  
-  def connect(self, endpoint):
-    self.socket.connect(endpoint)
-    self.servers += 1
-    print("%s: Connected to %s" % (self.client_id, endpoint))
-  
-  def request(self, *request):
-    msg = [b""] + list(request)
-    self.socket.send_multipart(msg)
+class RETURN_VALUE:
+    ERR_STRING = "ERROR"
+    SUCCESS_STRING = "SUCCESS"
 
-    poll = zmq.Poller()
-    poll.register(self.socket, zmq.POLLIN)
+    ERR_BYTES = ERR_STRING.encode()
+    SUCCESS_BYTES = SUCCESS_STRING.encode()
 
-    socks = dict(poll.poll(self.MAX_REQUESTS_TIMEOUT))
-    if socks.get(self.socket) == zmq.POLLIN:
-      reply = self.socket.recv_multipart()
-    else:
-      reply = None
-    return reply
 
-if __name__ == '__main__':
-  server_list = [
-    "tcp://192.168.18.71:5555",
-    "tcp://192.168.18.71:5557",
-  ]
-  
-  client = Client()
-  
-  endpoint = len(server_list) - 1
-  print("%s: Trying to connect to server..." % client.client_id)
-  client.connect(server_list[endpoint])
-  
-  while endpoint >= 0:
-    try :
-      # List Command
-      print("List of commands: ")
-      print("- HEALTH")
-      print("- LIST")
-      print("- LIST_ALL")
-      print("- DOWNLOAD <filename>")
-      print("- EXIT")
-      while True:
-        command = input("Enter your message: ")
-        if (input == "EXIT"):
-          raise Exception("Exiting the program")
-        
-        if command.startswith("DOWNLOAD"):
-          command = command.split(" ")
-          reply = client.request(command[0].encode(), command[1].encode())
-          
-          with open(f"client_{command[1]}", "wb") as f:
-            f.write(reply[0])
-          
-          print(f"Downloaded {command[1]}")
-          continue
-        else:
-          reply = client.request(command.encode())   
-          if not reply:
-            print("E: No response from server")
-            break
-          for i in range(len(reply)):
-            if i == 0:
-              print("Server: %s" % reply[i].decode())
+class Client:
+    MAX_RETRIES = 3
+    MAX_REQUESTS_TIMEOUT = 3000
+
+    def __init__(self, server_list: list[str]) -> None:
+        self.__server_list = server_list
+
+        # define the variable data type only
+        self.__context = zmq.Context()
+        self.__client: zmq.SyncSocket
+        self.__poller = zmq.Poller()
+        self.__sockets = dict()
+
+        self.client_id = "%04X-%04X" % (randint(0, 0x10000), randint(0, 0x10000))
+
+    def destroy(self):
+        self.__client.setsockopt(zmq.LINGER, 0)  # Terminate early
+        self.__client.close()
+        self.__context.term()
+
+    def send_request(self, *request):
+        reply = list()
+
+        for endpoint in self.__server_list:
+            server_reply: list[bytes] = []
+
+            for _ in range(client.MAX_RETRIES):
+                print("%s: Connecting to %s" % (self.client_id, endpoint))
+                self.__client = self.__context.socket(zmq.REQ)
+                self.__client.connect(endpoint)
+
+                msg = [self.client_id.encode()] + list(request)
+                self.__client.send_multipart(msg)
+
+                self.__poller.register(self.__client, zmq.POLLIN)
+                self.__sockets.update(self.__poller.poll(self.MAX_REQUESTS_TIMEOUT))
+
+                if self.__sockets.get(self.__client) == zmq.POLLIN:
+                    server_reply = self.__client.recv_multipart()
+
+                if server_reply:
+                    reply.append(server_reply)
+                    self.__poller.unregister(self.__client)
+                    break
+
+            if server_reply == []:
+                print(f"E: Server {endpoint} seems to be offline")
+
+        return reply
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print(f"E: Syntax: {sys.argv[0]} <server_endpoint>...")
+        sys.exit(1)
+
+    client = Client(sys.argv[1:])
+    print(f"I: Client started: ID: {client.client_id}")
+
+    while True:
+        # List Command
+        print("List of commands: ")
+        print("- HEALTH")
+        print("- LIST")
+        print("- LIST_ALL")
+        print("- DOWNLOAD <filename>")
+        print("- EXIT")
+        command = input("Enter your command: ")
+
+        try:
+            if command == "EXIT":
+                print("I: Exiting program...")
+                client.destroy()
+                os._exit(os.EX_OK)
+            elif command == "HEALTH":
+                res = client.send_request(command.encode())
+                for reply in res:
+                    print(f"M: server {reply[0].decode()} status: {reply[1].decode()}")
+                    print(f"M: reply: {reply[2].decode()}")
+            elif command == "LIST" or command == "LIST_ALL":
+                res = client.send_request(command.encode())
+                for reply in res:
+                    print(f"M: server {reply[0].decode()} status: {reply[1].decode()}")
+                    for filename in reply[2:]:
+                        print(f"M: Files: {filename.decode()}")
+            elif command.startswith("DOWNLOAD"):
+                command = command.split(" ")
+                
+                if len(command) != 3:
+                    print("E: DOWNLOAD <machine_name> <filename>")
+                    continue
+
+                res = client.send_request(
+                    command[0].encode(), command[1].encode(), command[2].encode()
+                )
+
+                if res[1] == RETURN_VALUE.ERR_BYTES:
+                    print(f"E: Cannot download the file: {res[2].decode()}")
+                else:
+                    with open(f"client_{command[1]}_{command[2]}", "wb") as f:
+                        f.write(res[0][0])
+
             else:
-              print("Message: %s" % reply[i].decode())
-      
-    except Exception as e:
-      print("E: Couldn't connect to server %s" % server_list[endpoint])
-      print("E: %s" % e)
-      endpoint -= 1
-      client.connect(server_list[endpoint])
-      continue
+                print("E: Invalid command")
+
+        except Exception as e:
+            print(f"E: {e}")
+            client.destroy()
+            sys.exit(1)
--- model_1/server.py
+++ model_1/server.py
@@ -16,16 +16,19 @@ class Server:
     SHARED_DIR = os.getcwd() + os.sep + "shared"
 
     def __init__(self, machine_name: str, endpoint: str) -> None:
-        self.__machine_name = machine_name
+        self.machine_name = machine_name
         self.__endpoint = endpoint
         self.__context = zmq.Context()
         self.__server = self.__context.socket(zmq.REP)
 
+        # bind the server
         self.__server.bind(endpoint)
 
     def destroy(self) -> None:
+        # destroy server
         self.__server.setsockopt(zmq.LINGER, 0)
         self.__server.close()
+
         self.__context.term()
 
     def list_all_files(self, include_hidden=False) -> list[bytes]:
@@ -53,39 +56,70 @@ class Server:
             print(f"I: Received message: {msg}")
 
             command = msg[1].decode()
+            client_id = msg[0].decode()
+
             if command == "HEALTH":
-                self.__server.send_multipart([RETURN_VALUE.SUCCESS_BYTES, b"OK"])
+                print(f"I: Sending reply to {client_id}")
+                self.__server.send_multipart(
+                    [self.machine_name.encode(), RETURN_VALUE.SUCCESS_BYTES, b"OK"]
+                )
             elif command == "LIST":
                 files = self.list_all_files()
+
+                print(f"I: Sending reply to {client_id}")
                 self.__server.send_multipart(
-                    [RETURN_VALUE.SUCCESS_BYTES, self.__machine_name.encode()] + files
+                    [self.machine_name.encode(), RETURN_VALUE.SUCCESS_BYTES] + files
                 )
             elif command == "LIST_ALL":
                 files = self.list_all_files(include_hidden=True)
+
+                print(f"I: Sending reply to {client_id}")
                 self.__server.send_multipart(
-                    [RETURN_VALUE.SUCCESS_BYTES, self.__machine_name.encode()] + files
+                    [self.machine_name.encode(), RETURN_VALUE.SUCCESS_BYTES] + files
                 )
             elif command == "DOWNLOAD":
-                # check if the filename is provided
-                if len(msg) != 3:
-                    self.__server.send_multipart([b"Filename not provided"])
-                    continue
+                machine = msg[2].decode()
+                filename = msg[3].decode()
 
-                filename = msg[2].decode()
+                if self.machine_name != machine:
+                    self.__server.send_multipart(
+                        [
+                            self.machine_name.encode(),
+                            RETURN_VALUE.ERR_BYTES,
+                            b"Not this one",
+                        ]
+                    )
+                    continue
 
                 # check if file exists
                 if not os.path.exists(self.SHARED_DIR + os.sep + filename):
-                    self.__server.send_multipart([b"File not found"])
+                    self.__server.send_multipart(
+                        [
+                            self.machine_name.encode(),
+                            RETURN_VALUE.ERR_BYTES,
+                            b"File not found",
+                        ]
+                    )
                     continue
 
-                # send the
+                print(f"I: Sending reply to {client_id}")
+
+                # send the file
                 with open(self.SHARED_DIR + os.sep + filename, "rb") as f:
                     data = f.read(1024)
                     while data:
                         self.__server.send_multipart([data])
                         data = f.read(1024)
+
             else:
-                self.__server.send_multipart([b"Invalid command"])
+                print(f"E: Invalid command, sending reply to {client_id}")
+                self.__server.send_multipart(
+                    [
+                        self.machine_name.encode(),
+                        RETURN_VALUE.ERR_BYTES,
+                        b"Invalid command",
+                    ]
+                )
 
         self.destroy()
 
